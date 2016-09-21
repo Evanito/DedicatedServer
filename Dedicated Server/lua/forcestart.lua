e4f0fe29cda61b35f79a448dac6496b53f4e9c25
Dr_Newbie <tf2baidonation@gmail.com>
2016-09-22 00:06:39 +0800

    4.1

--- Dedicated Server/lua/forcestart.lua
+++ Dedicated Server/lua/forcestart.lua
@@ -6,13 +6,14 @@ function MissionBriefingGui:update(t, dt)
 	local Game_Cancel_Hesit_Casue_Wait_Too_Long = DedicatedServer and DedicatedServer.Settings and DedicatedServer.Settings.Game_Cancel_Hesit_Casue_Wait_Too_Long or 60
 	local _Msg = DedicatedServer and DedicatedServer.Settings and DedicatedServer.Settings.Game_Announce_When_Ready_To_Start or {}
 	local tickkk = math.round(t) - 1
-	if tickkk >= 1 and tickkk <= 7 and not _MissionBriefingGui_update_announce[tickkk] and _Msg and _Msg[tickkk] then
+	local _Msg_Amount = _Msg and #_Msg or 0
+	if _Msg_Amount > 0 and  tickkk >= 1 and tickkk <= _Msg_Amount and not _MissionBriefingGui_update_announce[tickkk] and _Msg[tickkk] then
 		_MissionBriefingGui_update_announce[tickkk] = true
 		if managers.chat then
 			managers.chat:send_message(ChatManager.GAME, "[Auto-Looby]", _Msg[tickkk])
 		end
 	end
-	if t > 15 and not self._ready then
+	if t > 7+_Msg_Amount and not self._ready then
 		self:on_ready_pressed()
 	end
 	if Game_Cancel_Hesit_Casue_Wait_Too_Long >= 0 and t > Game_Cancel_Hesit_Casue_Wait_Too_Long and game_state_machine:current_state_name() ~= "disconnected" then
