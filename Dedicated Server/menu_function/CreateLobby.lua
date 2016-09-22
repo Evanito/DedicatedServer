53989b93d4dc8e2c09c3bfcd83186129ad712362
Dr_Newbie <tf2baidonation@gmail.com>
2016-09-22 17:50:43 +0800

    WIP.1.14

--- Dedicated Server/menu_function/CreateLobby.lua
+++ Dedicated Server/menu_function/CreateLobby.lua
@@ -2,11 +2,24 @@ _G.DedicatedServer = _G.DedicatedServer or {}
 
 DedicatedServer.Current_Server_Settings = {}
 
-function DedicatedServer:SetNextHeist(data)
+function DedicatedServer:SetNextHeist(data, retry)
 	local _Last_Lobby_Hesitcycle = self.Last_Data.Last_Lobby_Hesitcycle or 0
 	local _Lobby_Hesitcycle = self.Settings.Lobby_Hesitcycle or {}
 	local _Next_Hesit_Ready = {}
 	local _Next_Hesit = {}
+	retry = retry or 0
+	data = data or {}
+	
+	if retry > table.size(_Lobby_Hesitcycle) + 1 then
+		local _dialog_data = {
+			title = "Dedicated Server",
+			text = "Fail to create the heist\nReason: No required DLC.",
+			button_list = {{ text = "[OK]", is_cancel_button = true }},
+			id = tostring(math.random(0,0xFFFFFFFF))
+		}
+		managers.system_menu:show(_dialog_data)
+		return
+	end
 	
 	_Last_Lobby_Hesitcycle = _Last_Lobby_Hesitcycle + 1
 	if _Last_Lobby_Hesitcycle > #_Lobby_Hesitcycle then
@@ -20,12 +33,20 @@ function DedicatedServer:SetNextHeist(data)
 		_Next_Hesit = _Lobby_Hesitcycle[_Last_Lobby_Hesitcycle]
 	end
 	
-	if data and type(data) == "table" and #data > 0 then
+	if #data > 0 then
 		_Next_Hesit = data
 	end
 	
 	_Next_Hesit_Ready = self.Settings.Lobby_Default_Setting
 	_Next_Hesit_Ready.job = _Next_Hesit.job or _Next_Hesit_Ready.job
+	
+	local job_tweak_data = tweak_data.narrative.jobs[_Next_Hesit_Ready.job]
+	local is_not_dlc_or_got = not job_tweak_data.dlc or managers.dlc:is_dlc_unlocked(job_tweak_data.dlc)
+	if not is_not_dlc_or_got then
+		retry = retry + 1
+		self:SetNextHeist(nil, retry)
+	end
+	
 	_Next_Hesit_Ready.difficulty = _Next_Hesit.difficulty or _Next_Hesit_Ready.difficulty
 	_Next_Hesit_Ready.permission = _Next_Hesit.permission or _Next_Hesit_Ready.permission
 	_Next_Hesit_Ready.min_rep = _Next_Hesit.min_rep or _Next_Hesit_Ready.min_rep
@@ -37,7 +58,7 @@ function DedicatedServer:SetNextHeist(data)
 end
 
 function DedicatedServer:CreateLobby(data)
-	DedicatedServer.Current_Server_Settings = data or {}
+	self.Current_Server_Settings = data or {}
 	managers.menu:on_leave_lobby()
 	managers.job:on_buy_job(data.job, data.difficulty)
 	Global.game_settings.permission = data.permission ~= nil and data.permission or self.Settings.permission
