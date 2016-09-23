d47ae85d1f1dae0b466d7c7c9774cadd65c62f20
Dr_Newbie <tf2baidonation@gmail.com>
2016-09-24 00:38:53 +0800

    WIP.1.18

--- Dedicated Server/lua/notransferitemtome.lua
+++ Dedicated Server/lua/notransferitemtome.lua
@@ -1,8 +1,1 @@
-local _DedicatedServer_PlayerManager_transfer_special_equipment = PlayerManager.transfer_special_equipment
-
-function PlayerManager:transfer_special_equipment(peer_id, include_custody)
-	_DedicatedServer_PlayerManager_transfer_special_equipment(self, peer_id, include_custody)
-	if peer_id == 1 then
-		self:transfer_special_equipment(1)
-	end
-end
+--??
